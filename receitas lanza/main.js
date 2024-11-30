508caa6ce4e1400b93765cd8c6d25ecf68f845f4
AndreLanza-07 <blvlanza@gmail.com>
2024-11-30 16:55:34 -0300

    Add files via upload

---
+++ receitas lanza/main.js
@@ -0,0 +1,25 @@
+//let vl=document.getElementsByClassName('tot')
+//let qtd=document.getElementsByClassName('int')
+//let preco=document.getElementsByClassName('num')
+
+//function calcular(){
+  // let qtd= parseFloat(int.value) || 0;
+   //let preco = parseFloat(preco.value) || 0;
+  // vl.textContent = (qtd*preco).toFixed(2);
+//}
+//preco=parseFloat(preco.value);
+//qtd.addEventListener('input',calcular);
+//preco.addEventListener('input',calcular);
+
+
+
+
+//let linhas = document.querySelectorAll('tbody tr');
+
+ // linhas.forEach(linha => {
+  //  let preco = parseFloat(linha.querySelector('.num').textContent);
+  //  let quantidade = parseFloat(linha.querySelector('.int').value);
+  //  let total = (preco * quantidade);
+
+  //  linha.querySelector('.tot').textContent = total.toFixed(2);
+  //});
